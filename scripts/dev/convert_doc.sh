1b3e6924e912e87cd365955382c1c80d5d971d44
Fritz Reichwald <mail@fritzreichwald.de>
2018-12-29 17:37:50 +0100

    Add script to convert asciidoc to rst

---
+++ scripts/dev/convert_doc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# copyright by fiete
+# written at 35c3
+
+# this script creates a tmpdir
+#
+# then it changes there and uses asciidoctor to create docbook files from the original asciidoc documentation
+# in the end the docbook files are converted to rst using pandoc. Last the tmpdir is removed
+
+# requires asciidoctor and pandoc being installed and in the path
+
+# takes the files to convert as argument
+
+# go to doc/ directory in qutebrowser checkout and
+# run: $ ../scripts/dev/convert_doc.sh *.asciidoc help/*.asciidoc ../README.asciidoc
+
+tmpdir=$(mktemp -d)
+
+asciidoctor -b docbook5 -D "$tmpdir" "$@"
+for f in "$tmpdir"/*.xml; do
+    pandoc -f docbook -t rst -o "${f/.xml/}".rst "$f"
+done
+
+cp "$tmpdir"/*.rst .
+rm -r "$tmpdir"
